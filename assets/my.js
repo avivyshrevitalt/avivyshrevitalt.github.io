6853a19c462a02956d3af3c983e1d88a79db94f9
Aviv Yeshua <avivysh@gmail.com>
2021-02-11 14:46:25 +0200

    fixed text hebrew

--- assets/my.js
+++ assets/my.js
@@ -1,10 +1,12 @@
 $( document ).ready(function() {
-    jQuery.get('https://0d1i1j4e.api.sanity.io/v1/data/query/production?query=*[_type == "homePage"]{\'p1\':sectionD[]->{title},title,"header":header->{name,"image": image.asset->url,\'bioText\': bio[0].(children[0].text) }}',  // url
+    const url = 'https://0d1i1j4e.api.sanity.io/v1/data/query/production?query=*[_type == "homePage"]{\'p1\':sectionD[]->{title},title,"header":header->{name,"image": image.asset->url,\'bioText\': bio[].(children[0].text) },\'sectionA\':sectionA[]->{title,svg,description} }';
+    jQuery.get(url,
         function (data, textStatus, jqXHR) {  // success callback
             //alert('status: ' + textStatus + ', data:' + data);
 
             $('.content02 .display-1').html(data.result[0].header.name);
-            $('.content02 .text1').html(data.result[0].header.bioText.slice(0, 400) + "...");
+            $('.content02 .text1').html(data.result[0].header.bioText[0]+"\n\r\n\r"+data.result[0].header.bioText[1].slice(0, 400) );
+            $('.content02 .text2').html(data.result[0].header.bioText[2].slice(0, 100) + "...");
             setTimeout(function () {
                 console.log('run 1 sec image');
                 $('.content02 .i-amphtml-replaced-content').attr('src', data.result[0].header.image);
@@ -16,6 +18,6 @@ $( document ).ready(function() {
             //     tap:AMP.setState({ myImageUrl: data.result[0].header.image, myImageAlt: 'Another example image'});
             // })();
 
-            console.log(data.result[0].header.image);
+            console.log(data.result[0].header.bioText[0]+data.result[0].header.bioText[1]);
         });
 });
